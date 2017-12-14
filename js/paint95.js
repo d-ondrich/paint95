075ae622ea2a0190a43dd7575395e3401a988611
dbo1093 <dbo1093@gmail.com>
2017-12-14 15:00:51 +0200

    changed some style

--- js/paint95.js
+++ js/paint95.js
@@ -10,22 +10,23 @@ var paintBoardDiv = document.getElementById('paintBoard');
 paintBoardDiv.style = "width: 100%;";
 
 
-var canvas = document.createElement('table');
+var canvas = document.createElement('div');
 canvas.setAttribute('id', 'canvasArea');
-canvas.style = "background: blue; height: 500px; width: 500px; border: solid 2px black;";
+canvas.style = "background: white; height: 500px; width: 500px; border: solid 2px black;";
 paintBoardDiv.appendChild(canvas);
 
 
-for (var i = 0; i < 250; i++){
-    var row = document.createElement('tr');
-    row.setAttribute('id', 'row' + i);
-    canvas.appendChild(row);
-    for (var j = 0; j < 250; j++){
-        var cell = document.createElement('td');
-        cell.setAttribute('id', 'cell' + i + j);
-        cell.style = "background: white; height: 2px; width: 2px;";
-        row.appendChild(cell);
-    }
+// for (var i = 0; i < 250; i++){
+//     var row = document.createElement('div');
+//     row.setAttribute('id', 'row' + i);
+//     row.class = "rowStyle";
+//     canvas.appendChild(row);
+//     for (var j = 0; j < 250; j++){
+//         var cell = document.createElement('div');
+//         cell.setAttribute('id', 'cell' + i + j);
+//         cell.class = "cellStyle";
+//         row.appendChild(cell);
+//     }
     
-}
+// }
 
