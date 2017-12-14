e6f38efaacc57bef4616b21c5ab39147ca56f974
dbo1093 <dbo1093@gmail.com>
2017-12-14 14:31:36 +0200

    General layout looking nice

--- js/paint95.js
+++ js/paint95.js
@@ -3,23 +3,27 @@ functionalityWrapperDiv.setAttribute('id', 'functionalityWrapper');
 functionalityWrapperDiv.setAttribute('height', '300px');
 
 functionalityWrapperDiv.setAttribute('background-color', 'rgb(156, 154, 154);');
+
 document.getElementById('programHome').appendChild(functionalityWrapperDiv);
 
 var paintBoardDiv = document.getElementById('paintBoard');
+paintBoardDiv.style = "width: 100%;";
+
 
 var canvas = document.createElement('table');
 canvas.setAttribute('id', 'canvasArea');
-canvas.style = "background: blue; height: 600px; width: 600px;";
+canvas.style = "background: blue; height: 500px; width: 500px; border: solid 2px black;";
 paintBoardDiv.appendChild(canvas);
 
-for (var i = 0; i < 300; i++){
+
+for (var i = 0; i < 250; i++){
     var row = document.createElement('tr');
     row.setAttribute('id', 'row' + i);
     canvas.appendChild(row);
-    for (var j = 0; j < 300; j++){
+    for (var j = 0; j < 250; j++){
         var cell = document.createElement('td');
         cell.setAttribute('id', 'cell' + i + j);
-        cell.style = "background: blue; height: 2px; width: 2px;";
+        cell.style = "background: white; height: 2px; width: 2px;";
         row.appendChild(cell);
     }
     
