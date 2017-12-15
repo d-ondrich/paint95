3db0fc72839d7286a647d7589764255eb6591c0e
dbo1093 <dbo1093@gmail.com>
2017-12-15 20:29:05 +0200

    draw functionality working

--- js/paint95.js
+++ js/paint95.js
@@ -12,19 +12,40 @@ paintBoardDiv.style = "width: 100%;";
 
 var canvas = document.createElement('div');
 canvas.setAttribute('id', 'canvasArea');
-canvas.style = "background: white; height: 500px; width: 500px; border: solid 2px black;";
+canvas.style = "background: white; height: 500px; width: 500px; border: solid 2px black; position:relative;";
 paintBoardDiv.appendChild(canvas);
 
-var colorArray = ["black", "white", "blue", "red", "green", "orange", "pink", "purple", ]
+var colorArray = ["black", "white", "blue", "red", "green", "orange", "pink", "purple" ]
 
 for (var k = 0; k < 8; k++){
     var colorButton = document.createElement('button');
     colorButton.class = "colorButton";
-    colorButton.id = colorArray[k];
+    colorButton.backgroundColor = colorArray[k];
+    colorButton.id = colorArray[k] + k;
     colorButton.innerHTML = colorArray[k];
+    colorButton.addEventListener
     document.getElementById("colorPalette").appendChild(colorButton);
 }
 
+var paint = {};
+
+//paint.paintStroke = function(){
+    var canvasAreaVar = document.getElementById('canvasArea');
+    canvasAreaVar.addEventListener('click',paintColor);
+//};
+
+
+ function paintColor(e){
+    var canvasAreaVar = document.getElementById('canvasArea');
+        var brushDiv = document.createElement('div'); 
+        brushDiv.style.height = '5px';
+        brushDiv.style.width = '5px';
+        brushDiv.style.backgroundColor = 'black';
+        brushDiv.style.position = 'absolute';
+        brushDiv.style.top = e.pageY - this.offsetTop + "px";
+        brushDiv.style.left = e.pageX - this.offsetLeft + "px";
+        canvasAreaVar.appendChild(brushDiv);
+};
 
 
 // for (var i = 0; i < 250; i++){
