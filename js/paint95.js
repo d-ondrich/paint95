9c43307e2c5b9ccd9a1e9148d71db090dd352969
dbo1093 <dbo1093@gmail.com>
2017-12-16 19:45:23 +0200

    added brush size functionality and example display

--- js/paint95.js
+++ js/paint95.js
@@ -27,22 +27,22 @@ for (var k = 0; k < 8; k++){
     colorButton.addEventListener('click', colorSelection);
     document.getElementById("colorPalette").appendChild(colorButton);
 }
-var currentPaintBrushColor;
+var currentPaintBrushColor = 'black';
 function colorSelection(){
     if (this.id == 'eraserButton'){
         currentPaintBrushColor = 'white';
-        console.log("eraser working");
     } else {
     currentPaintBrushColor = this.id;
+    document.getElementById('brushSizeExample').style.backgroundColor = currentPaintBrushColor;
     }
 }
+var brushSize = '5px';
 
 var canvasAreaVar = document.getElementById('canvasArea');
 canvasAreaVar.addEventListener('click',paintColor);
 
-var brushSize = '5px';
 
- function paintColor(e){
+function paintColor(e){
     var canvasAreaVar = document.getElementById('canvasArea');
         var brushDiv = document.createElement('div'); 
         brushDiv.style.height = brushSize;
@@ -60,3 +60,51 @@ eraserButtonVar.innerHTML = "Eraser";
 eraserButtonVar.isSelected = false;
 eraserButtonVar.addEventListener('click', colorSelection);
 document.getElementById('paletteWrapper').appendChild(eraserButtonVar);
+
+var brushSizeWrapperVar = document.createElement('div');
+brushSizeWrapperVar.id = 'brushSizeWrapper';
+brushSizeWrapperVar.style = 'display: flex;'
+document.getElementById('paletteWrapper').appendChild(brushSizeWrapperVar);
+
+var plusButtonVar = document.createElement('button');
+plusButtonVar.id = 'plusButton';
+plusButtonVar.innerHTML = "+";
+plusButtonVar.addEventListener('click', increaseBrushSize);
+document.getElementById('brushSizeWrapper').appendChild(plusButtonVar);
+
+function increaseBrushSize(){
+    if (parseInt(brushSize) < 25){
+        brushSize = parseInt(brushSize) + 1;
+        brushSize = brushSize + 'px';
+        document.getElementById('brushSizeExample').style.height = brushSize;
+        document.getElementById('brushSizeExample').style.width = brushSize;
+    }
+}
+
+var brushSizeDisplayVar = document.createElement('div');
+brushSizeDisplayVar.style = 'height: 35px; width: 35px; background-color: white; border: solid 2px black;';
+brushSizeDisplayVar.style.display = 'flex';
+brushSizeDisplayVar.style.justifyContent = 'center';
+brushSizeDisplayVar.style.alignItems = 'center';
+brushSizeDisplayVar.id = 'brushSizeDisplay';
+document.getElementById('brushSizeWrapper').appendChild(brushSizeDisplayVar);
+
+var minusButtonVar = document.createElement('button');
+minusButtonVar.id = 'minusButton';
+minusButtonVar.innerHTML = "-";
+minusButtonVar.addEventListener('click', decreaseBrushSize);
+document.getElementById('brushSizeWrapper').appendChild(minusButtonVar);
+
+function decreaseBrushSize(){
+    if (parseInt(brushSize) > 1){
+        brushSize = parseInt(brushSize) - 1;
+        brushSize = brushSize + 'px';
+        document.getElementById('brushSizeExample').style.height = brushSize;
+        document.getElementById('brushSizeExample').style.width = brushSize;
+    }
+}
+
+var brushSizeExampleVar = document.createElement('div');
+brushSizeExampleVar.id = 'brushSizeExample';
+brushSizeExampleVar.style = 'height: 5px; width: 5px; background-color: black;';
+document.getElementById('brushSizeDisplay').appendChild(brushSizeExampleVar);
